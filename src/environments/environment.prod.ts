b179937eb334edffda011bad0a89b90f7c032bea
kikegstve <garciastave.enrique@gmail.com>
2021-05-31 13:10:47 -0500

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API : 'https://608adc0d737e470017b7410f.mockapi.io/api/v1',
+  testServeWait: 1000,
+  isTestServer: true,
 };
